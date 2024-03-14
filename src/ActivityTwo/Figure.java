a31302fc6c4e67f4a9ed42c59aeb18f1b25085a7
Sinhularity <zS22017021@estudiantes.uv.mx>
2024-03-14 10:37:54 -0600

    First commit

---
+++ src/ActivityTwo/Figure.java
@@ -0,0 +1,16 @@
+package ActivityTwo;
+
+public abstract class Figure {
+    private int amountSides;
+
+    public Figure(int amountSides) {
+        this.amountSides = amountSides;
+    }
+
+    public int getAmountSides() {
+        return amountSides;
+    }
+
+    public abstract int getArea ();
+    public abstract int getPermieter();
+}
